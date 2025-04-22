3737650be1a47c90270190cf6722adea326a79d8
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-04-22 22:44:32 +0600

    tailwind refresher folder added

---
+++ module_01/tailwind_css_refresher/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-mint-500 text-white text-3xl  text-center'>Welcome Tailwind V4</h1>
+    </div>
+  )
+}
+
+export default App
