fc1a55cb8b707c9619eb3015f995f1fa6a96424f
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-05-05 07:28:16 +0600

    react Practice

---
+++ module_01_3_Basic-React-Components/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Curly from "./components/curly.jsx";
+
+const App = () => {
+  //   const style1 = {
+  //     backgroundColor:'pink'
+  //   }
+  //   const style2 ={
+  //     backgroundColor: 'yellow'
+  //   }
+  //   let color = 'yellow';
+  // let newStyle = '';
+
+  // if(color === 'pink'){
+  //   newStyle = style1
+  // }
+  // else{
+  //   newStyle = style2
+  // }
+  return (
+    <div>
+      {/* <Test /> */}
+      {/* <Person /> */}
+      <Curly />
+    </div>
+  );
+};
+
+export default App;
