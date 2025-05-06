45ec98b5ccedf44926f32f731ae4d6e85b15b416
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-05-06 23:01:57 +0600

    practice

--- module_01_3_Basic-React-Components/src/App.jsx
+++ module_01_3_Basic-React-Components/src/App.jsx
@@ -1,29 +1,11 @@
 import React from "react";
-import PackingList from "./components/PackingList";
+import Clock from "./components/Clock";
+
 
 const App = () => {
-  //   const style1 = {
-  //     backgroundColor:'pink'
-  //   }
-  //   const style2 ={
-  //     backgroundColor: 'yellow'
-  //   }
-  //   let color = 'yellow';
-  // let newStyle = '';
-
-  // if(color === 'pink'){
-  //   newStyle = style1
-  // }
-  // else{
-  //   newStyle = style2
-  // }
   return (
-    
     <div>
-      {/* <Test /> */}
-      {/* <Person /> */}
-      {/* <Curly /> */}
-    <PackingList />
+      <Clock />
     </div>
   );
 };
