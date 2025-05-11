0a62330a2a543dc3ea94c58cc1bec9447bc408bb
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-05-11 12:43:32 +0600

    Understanding State: A Component's Memory practice done

---
+++ module_02_3_Understanding-State-A-Component's-Memory/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Gallery from './component/Gallery'
+import Form from './component/Form'
+import FeedbackForm from './component/FeedbackForm'
+import NewFeedback from './component/NewFeedback'
+
+const App = () => {
+  return (
+    <div>
+      {/* <Gallery/> */}
+      {/* <Form/> */}
+      {/* <FeedbackForm/> */}
+      <NewFeedback/>
+    </div>
+  )
+}
+
+export default App
---
+++ module_02_3_Understanding-State-A-Component's-Memory/src/component/NewFeedback.jsx
@@ -0,0 +1,8 @@
+export default function NewFeedback() {
+  function handleClick() {
+    const name = prompt("What is your name?");
+    alert(`Hello, ${name}!`);
+  }
+
+  return <button onClick={handleClick}>Greet</button>;
+}
