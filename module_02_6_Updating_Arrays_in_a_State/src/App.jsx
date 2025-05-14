7ae4aeb270e3f0acc4e57920e287e543ef31c073
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-05-14 23:12:17 +0600

    updating array in a state practice

---
+++ module_02_6_Updating_Arrays_in_a_State/src/App.jsx
@@ -0,0 +1,18 @@
+
+import AddingArray from './components/AddingArray'
+import List from './components/AddingArray'
+import RemovingArray from './components/RemovingArray'
+import TransformingArray from './components/TransformingArray'
+
+function App() {
+
+  return (
+    <div>
+     {/* <AddingArray/> */}
+     {/* <RemovingArray/> */}
+     <TransformingArray/>
+    </div>
+  )
+}
+
+export default App
