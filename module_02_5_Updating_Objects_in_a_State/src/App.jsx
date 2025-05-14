35241abcf58c9307fca7edaa9aff150ba7b615b8
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-05-14 07:09:40 +0600

    Updating_Objects_in_a_State practice

---
+++ module_02_5_Updating_Objects_in_a_State/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Form from './component/Form'
+import MovingDot from './component/MovingDot'
+import SimpleForm from './component/SimpleForm'
+
+function App() {
+
+  return (
+    <div>
+      {/* <MovingDot/> */}
+      {/* <Form/> */}
+      <SimpleForm/>
+    </div>
+  )
+}
+
+export default App
