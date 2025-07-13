5ecbad63a238fe4f6df1edd6b042a54106be02e5
Mohammad Ibrahim <77734109+IbrahimOhid@users.noreply.github.com>
2025-07-13 22:57:17 +0600

    Connect the event handlers practice done

--- module_03_React_State_Management_Deep_Dive/src/components/Form.jsx
+++ module_03_React_State_Management_Deep_Dive/src/components/Form.jsx
@@ -1,61 +1,64 @@
 import React, { useState } from 'react'
 
-const submitForm = (answer)=>{
-    return new Promise((resolve, reject)=>{
+const submitForm = (answer) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if(answer.toLowerCase() === 'dhaka'){
-                resolve()
-            }else{
-                reject(new Error('Good Guess but a Wrong Answer. Try Again!'))
+            if (answer.toLowerCase === 'Eidgaon') {
+                resolve();
+            } else {
+                reject(new Error('Good guess but a wrong answer. Try again!'))
             }
         }, 3000);
     })
 }
 
 const Form = () => {
-    // visual State: empty, typing, submitting, success, error
+    // visual state : typing, submitting, success
 
+    //mandatory state
+    const [answer, setAnswer] = useState('');
+    const [error, setError] = useState(null);
+    // visual state
+    const [status, setStatus] = useState('typing')
 
-    // mandatory State
-    const [answer, setAnswer] = useState(''); // empty
-    const [error, setError] = useState(null); // error
-
-    // visual State
-    const [status, setStatus] = useState('typing') // typing, submitting, success,
-
+    // handleTextChange
     const handleTextChange = (e) => {
-        setAnswer(e.target.value);
-        
+        setError(null)
+        setAnswer(e.target.value)
+    }
+    if (status === 'success') {
+        return <h1 className='mx-auto py-20 px-20 text-green-500 font-bold text-3xl'>That's right!</h1>
     }
 
-    const handleSubmit = async (e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
         setStatus('submitting')
-        try{
-            await submitForm(answer)
+        try {
+            await submitForm(answer);
             setStatus('success')
-        }catch(err){
+        }
+        catch (err){
             setStatus('typing')
             setError(err.message)
         }
     }
-
-
-    if (status === 'success') return <h1 className='text-green-400 text-3xl font-bold py-20 px-20'>That's right!</h1>
     return (
-        <div className='px-20 py-10 text-3xl leading-12'>
+        <div className='mx-auto container w-2/4 py-20'>
             <h2>City quiz</h2>
-            <p>What city is located on two continents?</p>
+            <p>
+                In which city is there a billboard that turns air into drinkable water?
+            </p>
             <form onSubmit={handleSubmit}>
-                <textarea value={answer} onChange={handleTextChange} className='border' disabled={status === 'submitting'} />
+                <textarea className='border' value={answer} onChange={handleTextChange} disabled={status === 'submitting'} />
                 <br />
-                <button className='bg-red-500 px-3 py-1 text-white rounded-lg' disabled={answer === ''|| status==='submitting'}>Submit</button>
+                <button className='bg-red-600 py-1 px-3 text-white rounded-3xl' disabled={answer === '' || status === 'submitting'} >
+                    Submit
+                </button>
                 {status === 'submitting' && <p>Loading...</p>}
                 {error && <p className='text-red-600'>{error}</p>}
+
             </form>
-
         </div>
-
     )
 }
 
