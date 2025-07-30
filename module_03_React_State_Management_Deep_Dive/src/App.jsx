be74ddc56af1d111a2ec60fdf77203be58887568
ibrahimohid <ibrahimohid77@gmail.com>
2025-07-30 18:17:09 +0600

    use reducer hook advance practice

--- module_03_React_State_Management_Deep_Dive/src/App.jsx
+++ module_03_React_State_Management_Deep_Dive/src/App.jsx
@@ -1,55 +1,18 @@
-import AddTask from './components/AddTask'
-import Counter from './components/Counter'
-import Form from './components/Form'
-import Pointer from './components/Pointer'
-import AnotherForm from './components/anotherForm'
-import TaskList from './components/TaskList'
-import TravelPlan from './components/TravelPlan'
-import UserInfoForm from './components/UserInfoForm'
-import Book from './components/book'
-import NewCounter from './components/NewCounter'
-import ComplexCounter from './components/ComplexCounter'
-// import { useState } from 'react'
-
-// import initialTasks from './data/data'
-
-
+import AddTask from "./components/AddTask";
+import Counter from "./components/Counter";
+import Form from "./components/Form";
+import Pointer from "./components/Pointer";
+import AnotherForm from "./components/anotherForm";
+import TaskList from "./components/TaskList";
+import TravelPlan from "./components/TravelPlan";
+import UserInfoForm from "./components/UserInfoForm";
+import Book from "./components/book";
+import NewCounter from "./components/NewCounter";
+import ComplexCounter from "./components/ComplexCounter";
+import TodoApp from "./components/TodoApp";
 
 function App() {
-  // const [tasks, setTasks] = useState(initialTasks);
 
-  // const getTaskId = (taskId)=>{
-  //   const maxId = taskId.reduce((prev, curr)=>{
-  //     prev && prev.id > curr.id ? prev.id : curr.id;
-  //   })
-  //   return maxId + 1;
-  // }
-
-  // const handleAddTask = (text)=>{
-  //   setTasks([
-  //     ...tasks,
-  //     {
-  //       id: getTaskId(tasks),
-  //       text: text,
-  //       done: false
-  //     }
-  //   ])
-  // }
-
-  // const handleChangeTask = (task)=>{
-  //   const nextTask = tasks.map((t)=>{
-  //     if(t.id === task.id){
-  //       return task;
-  //     }else{
-  //       return t;
-  //     }
-  //   })
-  //   setTasks(nextTask)
-  // }
-
-  // const handleDeleteTask = (taskId) =>{
-  //   setTasks(tasks.filter((t)=> t.id !== taskId.id));
-  // }
 
   return (
     <div>
@@ -59,15 +22,14 @@ function App() {
       {/* <UserInfoForm/> */}
       {/* <TravelPlan/> */}
       {/* <Counter/> */}
-      {/* <h1>Prague itinerary</h1>
-      <AddTask onAddTask = {handleAddTask}/>
-      <TaskList tasks={tasks} onChangeTask={handleChangeTask} onDeleteTask = {handleDeleteTask}/> */}
+     <TodoApp/>
+      
 
       {/* <Reducer/> */}
       {/* <Book/> */}
       {/* <NewCounter/> */}
-      <ComplexCounter/>
+      {/* <ComplexCounter/> */}
     </div>
-  )
+  );
 }
-export default App
+export default App;
---
+++ module_03_React_State_Management_Deep_Dive/src/components/TodoApp.jsx
@@ -0,0 +1,50 @@
+import React, { useReducer } from "react";
+import initialTasks from "../data/data";
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
+import taskReducer from "../reducers/taskReducer";
+
+const TodoApp = () => {
+  const [tasks, dispatch] = useReducer(taskReducer, initialTasks);
+  const getTaskId = (taskId) => {
+    const maxId = taskId.reduce((prev, curr) => {
+      prev && prev.id > curr.id ? prev.id : curr.id;
+    });
+    return maxId + 1;
+  };
+
+  const handleAddTask = (text) => {
+    dispatch({
+      type: "added",
+      text,
+      id: getTaskId(tasks),
+    });
+  };
+
+  const handleChangeTask = (task) => {
+    dispatch({
+      type: "changed",
+      task,
+    });
+  };
+
+  const handleDeleteTask = (taskId) => {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
+  };
+  return (
+    <div>
+      <h1>Prague itinerary</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </div>
+  );
+};
+
+export default TodoApp;
