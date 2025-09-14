c9ff15232a421383e01d051b1a70c868ff7b155d
ibrahimohid <ibrahimohid77@gmail.com>
2025-09-14 22:39:00 +0600

    start & stop function added stopwatch component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Counter from "./component/Counter";
+import Stopwatch from "./component/Stopwatch";
+
+function App() {
+  return (
+    <>
+      {/* <Counter /> */}
+      <Stopwatch/>
+    </>
+  );
+}
+
+export default App;
