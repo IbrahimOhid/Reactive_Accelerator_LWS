e4404818324f7e1081c582e5ac0d97caf443c4ce
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2026-01-04 22:21:42 +0600

    axios get post practice

--- module_5.1_API_Request-with-Axios-in-React/src/App.jsx
+++ module_5.1_API_Request-with-Axios-in-React/src/App.jsx
@@ -1,59 +1,21 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import axios from "axios";
-import Posts from "./components/Posts";
-// import initialData from './data/apiData'
+import React, { useEffect, useState } from "react";
+import { getPost } from "./api/PostApi";
 
-function App() {
-  const [posts, setPosts] = useState([]);
-  const [error, setError] = useState(null);
-
-  const handleDeletePost = () => {};
-
-  const handleEditPost = () => {};
-
-  const fetchPostData = async () => {
-    try {
-      const response = await axios.get("http://localhost:8000/posts");
-      if (response && response.data) {
-        setPosts(response.data);
-      }
-    } catch (err) {
-      if (err.response) {
-        setError(
-          `Error From Server Status: ${err.response.status} - Message: ${err.response.data}`
-        );
-      } else {
-        setError(err.message);
-      }
-    }
+const App = () => {
+  const [post, setPosts] = useState([]);
+  const getPostData = async () => {
+    const res = await getPost();
+    console.log(res);
   };
 
   useEffect(() => {
-    fetchPostData();
+    getPostData();
   }, []);
-
   return (
-    <div className="mx-auto container">
-      <div>
-        <h1>API Request With Axios..</h1>
-        <hr />{" "}
-        <Posts
-          posts={posts}
-          onDeletePost={handleDeletePost}
-          onEditPost={handleEditPost}
-        />
-        {error ? (
-          <div className="mt-20">
-            <hr />
-            <h1 className="bg-pink-400 py-3 px-3">{error}</h1>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
+    <div>
+      <h1>hello</h1>
     </div>
   );
-}
+};
 
 export default App;
