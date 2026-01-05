50f8be2cb927c9ec1ed8cefebcba119f4c9266fa
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2026-01-05 22:01:18 +0600

    post components added

--- module_5.1_API_Request-with-Axios-in-React/src/App.jsx
+++ module_5.1_API_Request-with-Axios-in-React/src/App.jsx
@@ -1,21 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { getPost } from "./api/PostApi";
+import React from 'react'
+import Post from './components/Post'
 
 const App = () => {
-  const [post, setPosts] = useState([]);
-  const getPostData = async () => {
-    const res = await getPost();
-    console.log(res);
-  };
-
-  useEffect(() => {
-    getPostData();
-  }, []);
   return (
     <div>
-      <h1>hello</h1>
+      <Post/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
+
