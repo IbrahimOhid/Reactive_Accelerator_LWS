4fd912f330295f1b3c0c1f3c98c4e8c3efaf0b71
ibrahimohid <ibrahimohid77@gmail.com>
2026-03-03 22:39:06 +0600

    registration page done

--- 5.8_How_to_do_Authentication_in_React_with_Firebase/src/App.jsx
+++ 5.8_How_to_do_Authentication_in_React_with_Firebase/src/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Registration from './pages/Registration';
+import Login from './pages/Login';
+import Reset from './pages/Reset';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/registration',
+    element: <Registration/>
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/reset',
+    element: <Reset/>
+  }
+])
 
 const App = () => {
   return (
-    <div>App</div>
+    <RouterProvider router={router}/>
   )
 }
 
--- 5.8_How_to_do_Authentication_in_React_with_Firebase/src/firebase.js
+++ 5.8_How_to_do_Authentication_in_React_with_Firebase/src/firebase.js
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -10,8 +9,19 @@ const firebaseConfig = {
   projectId: "guestbook-ac8ee",
   storageBucket: "guestbook-ac8ee.firebasestorage.app",
   messagingSenderId: "1013316229867",
-  appId: "1:1013316229867:web:7de50610c24c0225b4fcae"
+  appId: "1:1013316229867:web:7de50610c24c0225b4fcae",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export const registerWithEmailAndPassword = async (email, password)=>{
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user
+    return user
+  } catch (error) {
+    console.log(error)
+  }
+}
