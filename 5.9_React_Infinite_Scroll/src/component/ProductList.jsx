e9cac7ba313b7f2a288c2cb79a5713d9978d145f
ibrahimohid <ibrahimohid77@gmail.com>
2026-03-13 03:01:34 +0600

    infinte scroll practice ongoing

---
+++ 5.9_React_Infinite_Scroll/src/component/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductList = () => {
+  return (
+    <div>
+        <div>ProductList</div>
+        <div>Loading ProductList Data...</div>
+    </div>
+  )
+}
+
+export default ProductList
