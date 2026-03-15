970f51721d0c21ffc3b283d188fef98ce0621c7d
ibrahimohid <ibrahimohid77@gmail.com>
2026-03-15 22:58:45 +0600

    practice ongoing uncommon react hook

---
+++ Dive_into_Uncommon_React_Hooks/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GitHubProfile from './component/GitHubProfile'
+
+const App = () => {
+  return (
+    <div>
+      <GitHubProfile/>
+    </div>
+  )
+}
+
+export default App
---
+++ Dive_into_Uncommon_React_Hooks/src/hook/useGithub.js
@@ -0,0 +1,31 @@
+import { useDebugValue, useEffect, useState } from "react";
+
+export const useGithub = (username) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useDebugValue(`fetching Data ${username}`)
+
+  const fetchGithubData = async()=>{
+    setLoading(true)
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        const data = await response.json();
+        setUser(data)
+        setLoading(false)
+    } catch (error) {
+        setError(error)
+    }
+  }
+useEffect(()=>{
+    fetchGithubData();
+},[username]);
+return {
+    user, loading, error
+}
+};
+
+
+
+
