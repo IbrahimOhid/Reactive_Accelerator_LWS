717fcfb6d864010a431c52ce4ba1152ad964721c
ibrahimohid <ibrahimohid77@gmail.com>
2026-04-02 22:44:32 +0600

    redux practice ongoing

--- 5.14_React-redux/src/App.jsx
+++ 5.14_React-redux/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import Counter from "./components/Counter";
 
 const App = () => {
+  const { counters } = useSelector((state) => state.counters);
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
+      <div className="py-20 border shadow-2xl w-60 mx-auto mt-10">
+        {counters.map((counter) => (
+          <Counter count={counter.value} key={counter.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ 5.14_React-redux/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import countersReducer from '../features/counters/countersSlice'
+
+export const store = configureStore({
+    reducer:{
+        counters: countersReducer
+    }
+})
